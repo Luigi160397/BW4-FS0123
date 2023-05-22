a31b00eb8bf4c9559f1d9bc519845f8e14b2cdf4
Giuseppe Canzoneri <gcanzoneri6@gmail.com>
2023-05-22 15:34:09 +0200

    classi aggiunte

--- src/main/java/entities/Tappa.java
+++ src/main/java/entities/Tappa.java
@@ -1,5 +1,35 @@
 package entities;
 
-public class Tappa {
+import java.util.UUID;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class Tappa {
+	private UUID id;
+	
+    private String zonaPartenza;
+    private String zonaArrivo;
+    private int numeroVoltePercorsa;
+	public Tappa(String zonaPartenza, String zonaArrivo, int numeroVoltePercorsa) {
+		super();
+		this.zonaPartenza = zonaPartenza;
+		this.zonaArrivo = zonaArrivo;
+		this.numeroVoltePercorsa = numeroVoltePercorsa;
+	}
+	@Override
+	public String toString() {
+		return "Tappa [id=" + id + ", zonaPartenza=" + zonaPartenza + ", zonaArrivo=" + zonaArrivo
+				+ ", numeroVoltePercorsa=" + numeroVoltePercorsa + "]";
+	}
+    
+
+   
+
+   
 }
--- src/main/java/entities/Tratta.java
+++ src/main/java/entities/Tratta.java
@@ -1,5 +1,46 @@
 package entities;
 
-public class Tratta {
 
+import java.util.UUID;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+
+public class Tratta {
+	private UUID id;
+	private Mezzo mezzo;
+	private String stazionePartenza;
+	private String capolinea;
+	private double tempoEffettivoPercorrenza;
+    private Tappa tappa;
+    private double tempoMedioPercorrenza;
+	public Tratta(Mezzo mezzo, String stazionePartenza, String capolinea, double tempoEffettivoPercorrenza, Tappa tappa,
+			double tempoMedioPercorrenza) {
+		super();
+		this.mezzo = mezzo;
+		this.stazionePartenza = stazionePartenza;
+		this.capolinea = capolinea;
+		this.tempoEffettivoPercorrenza = tempoEffettivoPercorrenza;
+		this.tappa = tappa;
+		this.tempoMedioPercorrenza = tempoMedioPercorrenza;
+	}
+	@Override
+	public String toString() {
+		return "Tratta [id=" + id + ", mezzo=" + mezzo + ", stazionePartenza=" + stazionePartenza + ", capolinea="
+				+ capolinea + ", tempoEffettivoPercorrenza=" + tempoEffettivoPercorrenza + ", tappa=" + tappa
+				+ ", tempoMedioPercorrenza=" + tempoMedioPercorrenza + "]";
+	}
+
+  
+   
+
+    
+    
 }
+
