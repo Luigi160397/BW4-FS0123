ffce5c0ecb151dcab342fdaaefb0d23d1550c21f
DarioSalvatore <dariopay69@gmail.com>
2023-05-22 15:05:48 +0200

    Classi-tessera-distributore

--- src/main/java/entities/Distributore.java
+++ src/main/java/entities/Distributore.java
@@ -1,5 +1,42 @@
 package entities;
 
-public class Distributore {
+import java.util.UUID;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class Distributore {
+	
+	
+	@Id
+	@GeneratedValue
+	private UUID id;
+	private TipoDistributore tipoDistributore;
+	private StatoDistributore statoAttivita;
+	
+	public Distributore(TipoDistributore tipoDistributore, StatoDistributore statoAttivita) {
+		super();
+		
+		this.tipoDistributore = tipoDistributore;
+		this.statoAttivita = statoAttivita;
+	}
+	
+	public Distributore(TipoDistributore tipoDistributore) {
+		super();
+		
+		this.tipoDistributore = tipoDistributore;
+		this.statoAttivita = null;
+		
+	}
+	
+	
+	
 }
--- src/main/java/entities/Tessera.java
+++ src/main/java/entities/Tessera.java
@@ -1,5 +1,44 @@
 package entities;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
 public class Tessera {
 
+	
+	@Id
+	@GeneratedValue
+	private UUID id;
+	private String nome;
+	private String cognome;
+	private LocalDate dataNascita;
+	private LocalDate dataRilascio;
+	private LocalDate dataScadenza;
+	public Tessera(String nome, String cognome, LocalDate dataNascita, LocalDate dataRilascio,
+			LocalDate dataScadenza) {
+		super();
+		
+		this.nome = nome;
+		this.cognome = cognome;
+		this.dataNascita = dataNascita;
+		this.dataRilascio = dataRilascio;
+		this.dataScadenza = dataScadenza;
+	}
+	
+	
+	
+	
+	
+	
 }
