ac8d92aa28263943afbfc9aac3ad42e55a95c86d
VittorioVescio92 <123975369+VittorioVescio92@users.noreply.github.com>
2023-05-22 14:49:44 +0200

    +

--- src/main/java/entities/Bus.java
+++ src/main/java/entities/Bus.java
@@ -1,5 +1,21 @@
 package entities;
 
+import java.time.LocalDate;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Bus extends Mezzo {
+
+	public Bus(int capienza, StatoMezzo stato, LocalDate dataInizioServizio, LocalDate dataFineServizio,
+			LocalDate dataInizioManutenzione, LocalDate dataFineManutenzione, int numeroBigliettiVidimati, int tratta) {
+		super(capienza = 50, stato, dataInizioServizio, dataFineServizio, dataInizioManutenzione, dataFineManutenzione,
+				numeroBigliettiVidimati, tratta);
+
+	}
 
 }
