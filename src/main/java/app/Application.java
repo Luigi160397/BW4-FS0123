ab79a3e4af672b8ba058c2896fe10355f90616bc
Luigi160397 <luigi.difraia.97@gmail.com>
2023-05-23 08:27:46 +0200

    alti oggetti istanziati

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -11,6 +11,7 @@ import dao.TappaDAO;
 import dao.TesseraDAO;
 import dao.TicketDAO;
 import dao.TrattaDAO;
+import entities.Abbonamento;
 import entities.Biglietto;
 import entities.Bus;
 import entities.Distributore;
@@ -18,6 +19,7 @@ import entities.StatoDistributore;
 import entities.StatoMezzo;
 import entities.Tappa;
 import entities.Tessera;
+import entities.TipoAbbonamento;
 import entities.TipoDistributore;
 import entities.Tratta;
 import lombok.extern.slf4j.Slf4j;
@@ -40,22 +42,50 @@ public class Application {
 		TrattaDAO trattaDao = new TrattaDAO(em);
 
 		Tessera tessera1 = new Tessera("Paolo", "Bitta", LocalDate.of(1975, 3, 23), LocalDate.of(2023, 3, 10));
+		Tessera tessera2 = new Tessera("Aldo", "Baglio", LocalDate.of(1968, 7, 17), LocalDate.of(2023, 4, 12));
+
 		Distributore distributore1 = new Distributore(TipoDistributore.AUTOMATICO, StatoDistributore.ATTIVO);
+
 		Tappa tappa1 = new Tappa("Colosseo", "Fori Imperiali", 5);
+		Tappa tappa2 = new Tappa("Colosseo", "Cavour", 7);
+
 		Tratta tratta1 = new Tratta(null, "Colosseo", "Termini", 0.40, tappa1, 0.30);
+		Tratta tratta2 = new Tratta(null, "Colosseo", "Termini", 0.40, tappa2, 0.30);
+
 		Bus bus1 = new Bus(50, StatoMezzo.IN_SERVIZIO, LocalDate.of(2023, 10, 14), LocalDate.of(2023, 10, 17), null,
 				null, 15, tratta1);
+		Bus bus2 = new Bus(50, StatoMezzo.IN_SERVIZIO, LocalDate.of(2023, 10, 14), LocalDate.of(2023, 10, 17), null,
+				null, 15, tratta2);
+
 		Biglietto biglietto1 = new Biglietto(tessera1, LocalDate.of(2023, 10, 15), distributore1,
 				LocalDate.of(2023, 10, 16), bus1);
 
-		tessDao.save(tessera1);
+		Abbonamento abbonamento1 = new Abbonamento(tessera2, LocalDate.of(2023, 7, 16), distributore1,
+				LocalDate.of(2023, 7, 17), bus2, TipoAbbonamento.MENSILE);
+
+//		tessDao.save(tessera1);
+		tessDao.save(tessera2);
+
+//		distDao.save(distributore1);
 		distDao.save(distributore1);
-		tappaDao.save(tappa1);
-		trattaDao.save(tratta1);
-		mezzoDao.save(bus1);
-		ticDao.save(biglietto1);
-		tratta1.setMezzo(bus1);
-		trattaDao.update(tratta1);
+
+//		tappaDao.save(tappa1);
+		tappaDao.save(tappa2);
+
+//		trattaDao.save(tratta1);
+		trattaDao.save(tratta2);
+
+//		mezzoDao.save(bus1);
+		mezzoDao.save(bus2);
+
+//		ticDao.save(biglietto1);
+		ticDao.save(abbonamento1);
+
+//		tratta1.setMezzo(bus1);
+		tratta2.setMezzo(bus2);
+
+//		trattaDao.update(tratta1);
+		trattaDao.update(tratta2);
 
 		em.close();
 		emf.close();
